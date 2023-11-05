a6de4d5edc58ddd6917ef2441fadb7cf7d374c8a
angelahao6 <115599906+angelahao6@users.noreply.github.com>
2023-11-04 17:49:45 -0700

    Centered navbar and did basic styling

--- frontend/src/components/navigation/Navbar.jsx
+++ frontend/src/components/navigation/Navbar.jsx
@@ -1,32 +1,35 @@
 import { NavLink } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-                <img src="../../../src/assets/uc thrift 2.svg" alt="logo" />
-            </div>
-            <div>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/textbooks">Textbooks</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/clothes">Clothes</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/utility">Utility</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/misc">Misc</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/profile">Profile</NavLink>
-                    </li>
-                </ul>
+        <nav className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <img src="../../../src/assets/uc thrift 2.svg" alt="logo" />
+                </div>
+                <div className="nav-elements">
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/textbooks">Textbooks</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/clothes">Clothes</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/utility">Utility</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/misc">Misc</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/profile">Profile</NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
