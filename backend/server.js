6f767558ea96ea4d082a85331195c5e08a554449
Brandon Liang <brandonliang2002@outlook.com>
2023-11-19 00:11:22 -0800

    Untested Implimentation but should work

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
 mongoose.set('strictQuery', false);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const uploadsDir = './uploads';
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.use('/uploads', express.static('uploads'));
 
 connection = "mongodb+srv://<username>:<password>@ucthrift-dev.xpujbsq.mongodb.net"
 
@@ -20,7 +42,7 @@ mongoose
     .then (() => console. log("Connected to DB"))
     .catch (console.error);
 
-const Item = require('./models/item');
+const Item = require('./models/Item');
 const User = require('./models/user');
 
 app.listen(8080, () => console.log('Server listening on port 8080: http://localhost:8080'));
@@ -70,6 +92,41 @@ app.delete('/items/delete/:_id', async (req, res) => {
     const result = await Item.findByIdAndDelete(req.params._id);
 
     res.json(result);
+});
+
+// Add item images
+app.post('/items/upload-images', upload.array('images', 5), async (req, res) => {
+    const item = await Item.findById(req.body.itemId);
+    if (!item) {
+        return res.status(404).send('Item not found');
+    }
+
+    const imagePaths = req.files.map(file => `/uploads/${file.filename}`);
+    item.images.push(...imagePaths);
+    await item.save();
+
+    res.json({ message: 'Images uploaded successfully', imagePaths: item.images });
+});
+
+// Delete item images(individual)
+app.delete('/items/delete-image/:itemId/:imageName', async (req, res) => {
+    const item = await Item.findById(req.params.itemId);
+    if (!item) {
+        return res.status(404).send('Item not found');
+    }
+
+    const imageToDelete = `/uploads/${req.params.imageName}`;
+    item.images = item.images.filter(image => image !== imageToDelete);
+    await item.save();
+
+    fs.unlink(path.join(__dirname, imageToDelete), (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error deleting the file');
+        }
+    });
+
+    res.send('Image deleted successfully');
 });
     
 // USER ENDPOINTS
@@ -135,4 +192,38 @@ app.get('/users/posted-items', async (req, res) => {
     const postedItems = user.postedItems;
 
     res.json(postedItems);
+});
+
+// Add profile photo
+app.post('/users/upload-profile-picture', upload.single('image'), async (req, res) => {
+    const user = await User.findById(req.body.userId);
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
+
+    user.image = `/uploads/${req.file.filename}`;
+    await user.save();
+
+    res.json({ message: 'Profile picture uploaded successfully', imagePath: user.image });
+});
+
+// Delete profile photo
+app.delete('/users/delete-profile-picture/:userId', async (req, res) => {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
+
+    const imagePath = user.image;
+    user.image = "";
+    await user.save();
+
+    fs.unlink(path.join(__dirname, imagePath), (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error deleting the file');
+        }
+    });
+
+    res.send('Profile picture deleted successfully');
 });
