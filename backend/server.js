4ef088395eddfb47a0b20125f7d206e7e67562f8
Shiyu Ye <105144733+Monicaaawa@users.noreply.github.com>
2023-11-19 17:00:16 -0800

    Deleted duplicate user registration endpoints

--- backend/server.js
+++ backend/server.js
@@ -169,24 +169,6 @@ app.post('/login', async (req, res) => {
         }
     })
 });
-
-// Log in to user account
-app.post('/login', async (req, res) => {
-    const {email, password} = req.body;
-    User.findOne({email: email})
-    .then(user => {
-        if(user) {
-            if(user.password === password) {
-                res.json('Success');
-            } else {
-                res.json({'error': 'Incorrect password.'});
-            }
-        } else {
-            res.json({'error': 'Email address is not registered.'});
-            return;
-        }
-    })
-});
     
 // Create new user 
 app.post('/register', async (req, res) => {
