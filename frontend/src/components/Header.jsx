7b3605ea16d4bd5de3eb14139a2b8c974214efa2
angelahao6 <115599906+angelahao6@users.noreply.github.com>
2023-12-04 13:57:00 -0800

    Fix pagination with search and filter

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,11 @@
 import './Header.css';
 import { NavLink } from 'react-router-dom';
-import Searchbar from './search/Searchbar';
 
 function Header() {
     return (
         <div className="container">
             <NavLink to="/" className = "nav-link web"> <img src="../../../src/assets/ucthrift.svg" alt="logo" /> </NavLink>
             <NavLink to="/" className = "nav-link mobile"> <img src="/uct.png" alt="logo" /> </NavLink>
-            <Searchbar />
             <NavLink to="/profile" className = "nav-link"> <img src = "https://via.placeholder.com/45x45" alt = "placeholder" /> </NavLink>
         </div>
     )
