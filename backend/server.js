f79d32b136441228fa75fa426c3e5e28c6aa210f
Jenna Wang <jenna2004@gmail.com>
2023-12-04 20:47:41 -0800

    sigh oops connection leak pt 5 or something

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ function sendEmail(to, subject, html) {
     });
 }
 
-connection = "mongodb+srv://jayeNay:Ci23gVe5PJXFDmd8@ucthrift-dev.xpujbsq.mongodb.net"
+connection = "mongodb+srv://<username>:<password>@ucthrift-dev.xpujbsq.mongodb.net"
 
 // INIT CONNECTION
 mongoose
